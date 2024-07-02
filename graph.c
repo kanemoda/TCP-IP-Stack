d5bcfca6723228fda1555adb3ccd13670d98ba81
kanemoda <efedenizbaglar45@gmail.com>
2024-07-02 22:59:22 +0300

    create topology skeleton

--- graph.c
+++ graph.c
@@ -1,37 +1,14 @@
-#include "glthreads/glthread.h"
-#define NODE_NAME_SIZE 16
-#define IF_NAME_SIZE 16
-#define MAX_INTF_PER_NODE 10
+#include "graph.h"
+#include <stdlib.h>
+#include <string.h>
+#include "stdio.h"
 
-//Forward Declarations
-typedef struct node_ node_t;
-typedef struct link_ link_t;
-
-typedef struct interface_
+graph_t * create_new_graph(char * topology_name)
 {
-    char if_name[IF_NAME_SIZE];
-    struct node_ *att_node;
-    struct link_ *link;
-} interface_t;
+    graph_t *graph = calloc(1, sizeof(graph_t));
+    strncpy(graph->topology_name, topology_name , 32);
+    graph->topology_name[31] = '\0';
 
-struct link_
-{
-    interface_t intf1;
-    interface_t intf2;
-    unsigned int cost;
-};
-
-struct node_
-{
-    char node_name[NODE_NAME_SIZE];
-    interface_t *intf[MAX_INTF_PER_NODE];
-    glthread_t graph_glue;
-};
-
-
-
-typedef struct graph_
-{
-    char topology_name[32];
-    glthread_t node_list;
-};
+    init_glthread(&graph->node_list);
+    return graph;
+}
